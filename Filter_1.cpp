8502291d0ce8768297d5fcf69f6e1ca0dc85cda5
EslamKareem <eslam.karim.m@gmail.com>
2026-09-23 03:32:04 +0300

    small changes to explain some function and providing links to cheack out

--- Filter_1.cpp
+++ Filter_1.cpp
@@ -7,23 +7,22 @@ void applyfilterGrayscale(Image &img) {
       unsigned char r = img.getPixel(_x, _y, 0);
       unsigned char g = img.getPixel(_x, _y, 1);
       unsigned char b = img.getPixel(_x, _y, 2);
-      int grayscale = static_cast<int>(
-          (0.299 * r) + (0.587 * g) +
-          (0.114 *
-           b));
-// I could have used this "(pixel.r + pixel.g + pixel.b) / 3"
-// but it will look ugly so i did a bit of research (used google search ai
-// assistant while searching so i don`t waste a lot of time) and i found
-// this "https://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color"
-// and i got an interasting forumla that makes it like the
-// photoshop grayscale and it`s called Luminosity equation so it looks
-// better now and more realistic (=
+      int grayscale = static_cast<int>((0.299 * r) + (0.587 * g) + (0.114 * b));
+      // I could have used this "(pixel.r + pixel.g + pixel.b) / 3"
+      // but it will look ugly so i did a bit of research (used google search ai
+      // assistant while searching so i don`t waste a lot of time) and i found
+      // this
+      // "https://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color"
+      // and i got an interasting forumla that makes it like the
+      // photoshop grayscale and it`s called Luminosity equation so it looks
+      // better now and more realistic (=
       img.setPixel(_x, _y, 0, grayscale);
       img.setPixel(_x, _y, 1, grayscale);
       img.setPixel(_x, _y, 2, grayscale);
     }
   }
 }
+//***//***//***//***//***//***//***//***//***//***//***//***//***//***//
 // well after not working that i didn`t notice why it kept giving me compiler
 // errors it was just this int grayscale more spicific this (int) yeah am an
 // idiot, anyways it was an easy fix if i did do some search i found a funny
