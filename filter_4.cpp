352513e15291411af36cc87717b01e43583a957b
karemmadi621-hue <karemmadi621@gmail.com>
2026-09-26 01:08:53 +0300

    Implement frame addition to images in filter_4.cpp
    This file implements a function to add a frame to an image based on user-defined thickness and color. It also includes a main function to load an image and save it after applying the frame.

---
+++ filter_4.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+ #include "image_class.h"
+using namespace std;
+
+void addframe( Image&  img)
+{
+	cout << "welcome to our application ";
+	cout << " enter frame thickness and color \n ";
+	int frameWidth;
+	cin >> frameWidth;
+	int framecolorR = 255;
+	int framecolorG = 255;
+	int framecolorB = 255;
+
+	int width = img.width;
+	int height = img.height;
+
+
+	for (int i = 0; i < height ;++i) 
+	{
+
+		for (int j = 0; j < width; ++j)
+			if (i < frameWidth || i >= height - frameWidth || j < frameWidth || j >= width - frameWidth) 
+			{
+				img.setPixel(j, i, 0, framecolorR);
+				img.setPixel(j, i, 1, framecolorG);
+				img.setPixel(j, i, 2, framecolorB);
+			}
+			cout<<"frame added successfuiiy\n";     
+
+	} 
+
+	int main();  
+	{
+		string filename;
+			cout << "Enter image filename to load ";
+			cin >> filename;
+			  
+			  
+			Image img(filename);
+
+
+
+
+			addframe(img);
+
+			
+
+
+			string outputfilname;
+			cout << "Enter outputfilename to save";
+
+
+			cin >> outputfilname;
+			img.saveImage(outputfilname);
+
+			return 0;
+
+
+	}
+
+
+}
+vkak
