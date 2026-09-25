83ed2c3b193ce2044e21a862bae1915012422aae
EslamKareem <eslam.karim.m@gmail.com>
2026-09-26 02:14:37 +0300

    the last fix after solving the issue, kareem please fix the code when yyou wake up

--- filter_4.cpp
+++ filter_4.cpp
@@ -1,63 +1,44 @@
+#include "Image_Class.h"
 #include <iostream>
- #include "image_class.h"
 using namespace std;
 
-void addframe( Image&  img)
-{
-	cout << "welcome to our application ";
-	cout << " enter frame thickness and color \n ";
-	int frameWidth;
-	cin >> frameWidth;
-	int framecolorR = 255;
-	int framecolorG = 255;
-	int framecolorB = 255;
+void addframe(Image &img) {
+  cout << "welcome to our application ";
+  cout << " enter frame thickness and color \n ";
+  int frameWidth;
+  cin >> frameWidth;
+  int framecolorR = 255;
+  int framecolorG = 255;
+  int framecolorB = 255;
 
-	int width = img.width;
-	int height = img.height;
+  int width = img.width;
+  int height = img.height;
 
+  for (int i = 0; i < height; ++i) {
 
-	for (int i = 0; i < height ;++i) 
-	{
+    for (int j = 0; j < width; ++j)
+      if (i < frameWidth || i >= height - frameWidth || j < frameWidth ||
+          j >= width - frameWidth) {
+        img.setPixel(j, i, 0, framecolorR);
+        img.setPixel(j, i, 1, framecolorG);
+        img.setPixel(j, i, 2, framecolorB);
+      }
+    cout << "frame added successfuiiy\n";
+  }
 
-		for (int j = 0; j < width; ++j)
-			if (i < frameWidth || i >= height - frameWidth || j < frameWidth || j >= width - frameWidth) 
-			{
-				img.setPixel(j, i, 0, framecolorR);
-				img.setPixel(j, i, 1, framecolorG);
-				img.setPixel(j, i, 2, framecolorB);
-			}
-			cout<<"frame added successfuiiy\n";     
+  int main = 0;
+  {
+    string filename;
+    cout << "Enter filename to load image:";
+    cin >> filename;
+    Image img(filename);
 
-	} 
+    addframe(img);
 
-	int main();  
-	{
-		string filename;
-			cout << "Enter image filename to load ";
-			cin >> filename;
-			  
-			  
-			Image img(filename);
+    string outputfilname;
+    cout << "Enter outputfilename to save";
 
-
-
-
-			addframe(img);
-
-			
-
-
-			string outputfilname;
-			cout << "Enter outputfilename to save";
-
-
-			cin >> outputfilname;
-			img.saveImage(outputfilname);
-
-			return 0;
-
-
-	}
-
-
+    cin >> outputfilname;
+    img.saveImage(outputfilname);
+  }
 }
