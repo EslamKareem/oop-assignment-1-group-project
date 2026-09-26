afe0662170acabc1bfd6745c0555d3a0c2da7d11
oysabdo07 <oysabdo07@gmail.com>
2026-09-26 19:05:36 +0300

    changed file name

---
+++ filter2.cpp
@@ -0,0 +1,26 @@
+#include "Image_Class.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  Image image("luffy.jpg");
+  for (int i = 0; i < image.width; i++) {
+    for (int j = 0; j < image.height; j++) {
+      unsigned int avg = 0;
+      for (int k = 0; k < 3; k++) {
+        avg += image(i, j, k);
+      }
+      avg /= 3;
+      if (avg >= 128) {
+        for (int k = 0; k < 3; k++) {
+          image(i, j, k) = 255;
+        }
+      } else {
+        for (int k = 0; k < 3; k++) {
+          image(i, j, k) = 0;
+        }
+      }
+    }
+  }
+  image.saveImage("luffy2.jpg");
+}
